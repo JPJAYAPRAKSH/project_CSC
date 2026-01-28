897ecf76f31c965bbcc13e8cc529467fc4a8a7b8
JP <146056756+JPJAYAPRAKSH@users.noreply.github.com>
2026-01-28 19:59:37 +0530

    Prepare for Deployment

--- backend/institute_system/settings.py
+++ backend/institute_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k2ff2yu5f3=&c3u5@d2oam@gy3&fu-#cu%(#*19-t$(*+^12dg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
